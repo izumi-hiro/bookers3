a9c133e5796116161f8d764c8ae9c18528284fc9
izumi-hiro <transformers2728frost@gmail.com>
2022-05-09 08:05:15 +0000

    [Add] 36

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,17 +10,19 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-  def edit
-  end
-
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
     redirect_to book_path(@book)
   end
+  
+  def edit
+  end
 
   def update
+    @book.update(book_params)
+    redirect_to book_path(@book)
   end
 
   def destroy
