87e53538ca864024674c13ab89131a1042586c4e
izumi-hiro <transformers2728frost@gmail.com>
2022-05-09 12:25:16 +0000

    [Add] 44

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,8 +25,11 @@ class BooksController < ApplicationController
   end
 
   def update
-    @book.update(book_params)
-    redirect_to book_path(@book)
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
   end
 
   def destroy
