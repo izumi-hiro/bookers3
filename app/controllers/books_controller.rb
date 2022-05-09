deb72d9a472d40536ad8f70e1afd81ac5ce76876
izumi-hiro <transformers2728frost@gmail.com>
2022-05-09 12:22:58 +0000

    [Add] 43

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,8 +13,12 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book)
+    if @book.save
+      redirect_to book_path(@book), notice: "You have created book successfully."
+    else
+      @books = Book.all
+      render 'index'
+    end
   end
   
   def edit
