eab284f12763c0a153c583735d41ccb5a9be45e6
izumi-hiro <transformers2728frost@gmail.com>
2022-05-10 11:26:37 +0000

    [Start] o-4

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   has_many :books, dependent: :destroy
+  
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  
   has_one_attached :profile_image
   
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
@@ -18,5 +26,17 @@ class User < ApplicationRecord
         profile_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+  
+  def follow(user)
+    relationships.create(followed_id: user.id)
+  end
+
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
   end
 end
