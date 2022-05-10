02d42478be3d8d7da90cc22978d3cbc5fb61604a
izumi-hiro <transformers2728frost@gmail.com>
2022-05-10 08:14:38 +0000

    [Start] o-3 favorite

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => 'homes#about', as: 'about'
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
   resources :users, only: [:index, :show, :edit, :update]
